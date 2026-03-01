fad187c07a4ec2515b1ce4983c15e948ea10a5ed
Nitish C <86357181+Nitish-bot@users.noreply.github.com>
2026-03-02 00:13:40 +0530

    add lint + fmt scripts and configure vite for deployment

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,20 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    base: '/samizdat/',
+    plugins: [
+        react(),
+        tailwindcss(),
+        nodePolyfills({
+            globals: {
+                Buffer: true,
+            },
+        }),
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
